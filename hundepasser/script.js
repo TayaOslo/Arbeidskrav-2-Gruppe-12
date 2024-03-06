ab5f0261cd756cf59fd92b2821f23ab095efc8ff
Yousuf <bashiyow@gmail.com>
2024-03-06 20:42:56 +0100

    oppretter kort

--- hundepasser/script.js
+++ hundepasser/script.js
@@ -97,5 +97,13 @@ async function loadProfileCards(filterBreed) {
   profilesContainer.innerHTML = ""; // Tøm eksisterende kort
 
   // Last inn 10 nye kort basert på filteret
-  for (let i = 0; i < 10; i++) {}
+  for (let i = 0; i < 10; i++) {
+    const user = await fetchRandomUser();
+    const dogImage = await fetchRandomDogImage();
+
+    // Opprett kortet hvis det ikke er et filter eller hunderase matcher
+    if (!filterBreed || user.name.first.toLowerCase().includes(filterBreed)) {
+      createProfileCard(user, dogImage);
+    }
+  }
 }
